11e3d3f3aab26ac79dd6ffe1efa38c18e9afd13e
cs3k3 <csekebotar@gmail.com>
2023-10-09 17:25:03 +0200

    new post feature allowed now

--- src/main/java/com/cseke/springblog/controllers/PostController.java
+++ src/main/java/com/cseke/springblog/controllers/PostController.java
@@ -1,12 +1,16 @@
 package com.cseke.springblog.controllers;
 
+import com.cseke.springblog.models.Account;
 import com.cseke.springblog.models.Post;
+import com.cseke.springblog.services.AccountService;
 import com.cseke.springblog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -16,6 +20,8 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private AccountService accountService;
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
         Optional<Post> optionalPost = postService.getById(id);
@@ -26,6 +32,25 @@ public class PostController {
         } else {
             return "404";
         }
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model){
+        Optional<Account> optionalAccount=accountService.findByEmail("user1@email.com");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post",post);
+            return "post_new";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
     }
 
 }
