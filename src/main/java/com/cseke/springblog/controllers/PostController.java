022ca9737399440f8f1dc7b34b2a777684e2be66
cs3k3 <csekebotar@gmail.com>
2023-10-16 16:55:22 +0200

    done?

--- src/main/java/com/cseke/springblog/controllers/PostController.java
+++ src/main/java/com/cseke/springblog/controllers/PostController.java
@@ -5,8 +5,10 @@ import com.cseke.springblog.models.Post;
 import com.cseke.springblog.services.AccountService;
 import com.cseke.springblog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +53,52 @@ public class PostController {
     public String saveNewPost(@ModelAttribute Post post) {
         postService.save(post);
         return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    @PreAuthorize("isAuthenticated()")
+    public String getPostForEdit(@PathVariable Long id, Model model) {
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post_edit";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updatePost(@PathVariable Long id, Post post, BindingResult result, Model model) {
+
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post existingPost = optionalPost.get();
+
+            existingPost.setTitle(post.getTitle());
+            existingPost.setBody(post.getBody());
+
+            postService.save(existingPost);
+        }
+
+        return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/{id}/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String deletePost(@PathVariable Long id) {
+
+        // find post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+
+            postService.delete(post);
+            return "redirect:/";
+        } else {
+            return "404";
+        }
     }
 
 }
